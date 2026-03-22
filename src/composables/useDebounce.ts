36275c4fd719f150f68bf0287d4bde73f04f89f6
Kirill Shevtsov <kirwork.em@gmail.com>
2026-03-22 22:57:24 +0200

    refactoring: useDebounce ref source instead value

--- src/composables/useDebounce.ts
+++ src/composables/useDebounce.ts
@@ -1,19 +1,16 @@
-import { ref, watch } from 'vue'
+import { ref, type Ref, watch } from 'vue'
 
-export function useDebounce<T> (value: T, delay = 300) {
-  const debounced = ref(value) as { value: T }
+export function useDebounce<T> (source: Ref<T>, delay = 300) {
+  const debounced = ref(source.value) as Ref<T>
 
   let timeout: ReturnType<typeof setTimeout>
 
-  watch(
-    () => value,
-    newVal => {
-      clearTimeout(timeout)
-      timeout = setTimeout(() => {
-        debounced.value = newVal
-      }, delay)
-    },
-  )
+  watch(source, newVal => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      debounced.value = newVal
+    }, delay)
+  })
 
   return debounced
 }
